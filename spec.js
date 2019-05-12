73361e589d745fb11e25deb44f28244271593e02
ksr229 <sushankrana229@gmail.com>
2019-05-12 23:17:32 +1000

    first test added-spec.js and conf.js created

--- spec.js
+++ spec.js
@@ -1,1 +1,20 @@
-console.log("HelloWorld");
+
+describe('angularjs homepage todo list', function() {
+    it('should add a todo', function() {
+      browser.get('https://angularjs.org');
+  
+      element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+      element(by.css('[value="add"]')).click();
+       
+      var todoList = element.all(by.repeater('todo in todoList.todos'));
+      expect(todoList.count()).toEqual(3);
+      console.log("step1:Pass");
+      expect(todoList.get(2).getText()).toEqual('write first protractor test');
+      console.log("step2:Pass");
+      // You wrote your first test, cross it off the list
+      todoList.get(2).element(by.css('input')).click();
+      var completedAmount = element.all(by.css('.done-true'));
+      expect(completedAmount.count()).toEqual(2);
+      console.log("TEST:Pass");
+    });
+  });
